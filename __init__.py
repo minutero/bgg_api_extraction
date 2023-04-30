5ab5d47af46191670dd072569ca7776747a11088
Felipe Silva <silva.felipe@externos-cl.cencosud.com>
2023-04-30 14:26:30 -0400

    get top 100 network

--- __init__.py
+++ __init__.py
@@ -1,35 +1,26 @@
 import os
-from modules.db import db_init_, run_query
+import logging
+import requests
+from bs4 import BeautifulSoup
 from modules.designers import get_designers, get_games_from_designer
 from modules.config import database, designer_url
-import logging
+from modules.db import db_init, save_list_network_to_db
+from config.db_connection import run_query
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(getattr(logging, os.getenv("LOG_LEVEL", "INFO")))
 
 
-db_init_(database)
+db_init(database)
 get_designers(designer_url)
-query = f"""select distinct designer
-            from designers"""
-df_designers = run_query(query, execute_only=False)
-count_designer = len(df_designers)
-i = 0
-for designer in df_designers.to_dict("records"):
-    i += 1
-    logger.info("###################################################")
-    logger.info(
-        f"Designer {str(i).zfill(2)}/{str(count_designer).zfill(2)}: Processing {designer['designer']}"
-    )
-    logger.info("###################################################")
-    game_count = run_query(
-        "select count(1) from boardgame where designer = ?",
-        execute_only=False,
-        parameters=[designer["designer"]],
-    ).loc[0][0]
-    if (
-        game_count > 4
-    ):  # IMPROVEMENT: Find a better way on how to avoid rerunning a designer
-        continue
-    get_games_from_designer(designer["designer"])
+
+url = "https://boardgamegeek.com/browse/boardgame"
+req = requests.get(url)
+html = req.content
+bs = BeautifulSoup(html, features="html.parser")
+games_top100 = bs.table.findAll(
+    lambda tag: tag.name == "a" and tag.has_attr("href") and tag.has_attr("class")
+)
+list_games_id = [k["href"].split("/")[2] for k in games_top100]
+save_list_network_to_db(list_games_id)
