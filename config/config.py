03b096e2702fdfd33a3ff0fdb574292ec8c24eb0
Felipe Silva <silva.felipe@externos-cl.cencosud.com>
2023-05-14 14:52:21 -0400

    improvements

--- config/config.py
+++ config/config.py
@@ -1,4 +1,18 @@
 designer_url = "https://boardgamegeek.com/browse/boardgamedesigner"
 url_base = "https://boardgamegeek.com/xmlapi2/"
-url = "https://boardgamegeek.com/xmlapi/collection/minutero"
-parameters = {"rated": 1}
+
+columns = [
+    "id",
+    "name",
+    "weight",
+    "rating",
+    "year",
+    "type",
+    "minplayers",
+    "maxplayers",
+    "age",
+    "minplaytime",
+    "maxplaytime",
+    "rating_users",
+    "weight_users",
+]
